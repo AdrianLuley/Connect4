32fd61c046e9e9a47d8c6e48789ff45ec4d105f7
Adrian Luley <amluley@comcast.net>
2018-02-26 15:56:48 -0500

    First

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 let currentColor = "red";
 let currentPlayer = 1;
 let board = [
-    [0, 0, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
 ];
+// let board = [
+//     [0, 0, 1, 1, 1, 1],
+//     [0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0],
+//     [0, 1, 0, 0, 0, 0],
+//     [0, 1, 0, 0, 0, 0],
+//     [0, 1, 0, 0, 0, 0],
+//     [0, 1, 0, 0, 0, 0]
+// ];
 
 
-handleClick = function (event) {
-
+function handleClick(event) {
     const currentCol = event.currentTarget;
-    const colIndex = currentCol.id.split('-')[1];
     createBox(currentCol);
-    
+    const winner = checkWinner();
+    if(winner){
+        alert("Player " + winner + " Wins!");
+    }
+   
     if (currentColor == "red") {
         currentColor = "black";
         currentPlayer = 2;
@@ -39,7 +50,7 @@ for (let iCol = 0; iCol < 7; iCol++) {
 
 function createBox(col) {
     const colIndex = col.id.split("-")[1];
-    const rowIndex = document.querySelectorAll('#' + col.id + ' .row').length
+    const rowIndex = document.querySelectorAll('#' + col.id + ' .row').length;
 
     const row = document.createElement("div");
     row.classList.add("row");
@@ -47,34 +58,82 @@ function createBox(col) {
 
     row.id = "row-" + rowIndex;
 
-    console.log('colIndex is ', colIndex, ' and rowIndex is ', rowIndex)
-    console.log(board)
     board[colIndex][rowIndex] = currentPlayer;
 
-    col.appendChild(row)
+
+    col.appendChild(row);
 }
 
 
-const edgeX = board[0].length - 2;
-const edgeY = board.length - 2;
+function checkWinner() {
 
-for (var x = 0; x < edgeX; x++) {
-    let column = board[y];
-  
-    for (var y = 0; y < edgeY; y++) {
-        var row = board[x];
-        
-        if (row !== 0)
-            var horizontal = [
-                (board[x] || [])[y + 1],
-                (board[x] || [])[y + 2],
-                (board[x] || [])[y + 3]
-            ].every(function (c) {
-                return c === row;
-            });
-        if (horizontal) {
-            return true;
-            console.log("4 in a row found at" + [y+1] + [y+2] + [y+3]);
-    }
+
+    for (var x = 0; x < board.length; x++) {
+        let column = board[x];
+
+        for (var y = 0; y < column.length; y++) {
+            var cell = column[y];
+
+            if (cell !== 0) {
+                var horizontal = [
+                    (board[x + 1] || [])[y],
+                    (board[x + 2] || [])[y],
+                    (board[x + 3] || [])[y]
+                ].every(function (c) {
+                    return c === cell;
+                });
+                
+                if (horizontal) {
+                    return cell;
+
+                }
+
+                
+            } else if (cell !== 0) {
+                var vertical = [
+                    (board[x] || [])[y + 1],
+                    (board[x] || [])[y + 2],
+                    (board[x] || [])[y + 3]
+                ].every(function (c) {
+                    return c === cell;
+                });
+                if (vertical) {
+                    return cell;
+                }
+            } else if (cell !== 0) {
+                var diagonalRight = [
+                    (board[x + 1] || [])[y + 1],
+                    (board[x + 2] || [])[y + 2],
+                    (board[x + 3] || [])[y + 3]
+                ].every(function(c) {
+                    return c === cell;
+                });
+                if (diagonalRight) {
+                    return cell;
+                }
+            } else if (cell !== 0) {
+                var diagonalLeft = [
+                    (board[x - 1] || [])[y - 1],
+                    (board[x - 2] || [])[y - 2],
+                    (board[x - 3] || [])[y - 3]
+                ].every(function(c) {
+                    return c === cell;
+                });
+                if (diagonalLeft) {
+                    return cell;
+                }
+            }
+        }
     }
 }
+
+// const horizontal = [
+//     (grid[x + 1] || [])[y],
+//     (grid[x + 2] || [])[y],
+//     (grid[x + 3] || [])[y]
+// ].every(function (c) {
+//     return c === cell;
+// });
+// if (horizontal) {
+//     return cell;
+// }
